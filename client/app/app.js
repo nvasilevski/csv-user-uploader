a88fa22478e865f66be74e4d5d9e6e9fa56fa9b6
Nikita Vasilevsky <nvasilevskii@gmail.com>
2019-09-19 19:07:46 +0300

    Add basic clients view

--- client/app/app.js
+++ client/app/app.js
@@ -1,25 +1,47 @@
-import CONFIG from 'config';
+const CONFIG = {
+  host: "http://localhost:3000"
+}
 
-angular
-  .module('myApp', ['angularFileUpload'])
-  .directive('fileUpload', function(FileUploader) {
-    return {
-      restrict: 'E',
-      templateUrl:'components/file_upload.html',
-      compile: function() {
-        return {
-          pre: function(scope, element, attrs) {
-            var uploader = new FileUploader({url: CONFIG['host'] + '/clients/upload'});
-            uploader.onAfterAddingFile = function(item, filter, options) {
-              if (uploader.queue.length > 1) {
-                 uploader.queue.splice(0, 1);
-              }
-            };
-
-           scope.uploader = uploader;
-          },
-        };
-      }
+var app = angular.module('myApp', ['ngResource', 'angularFileUpload']);
+  
+app.directive('fileUpload', function(FileUploader) {
+  return {
+    restrict: 'E',
+    templateUrl:'components/file_upload.html',
+    compile: function() {
+      return {
+        pre: function(scope, element, attrs) {
+          var uploader = new FileUploader({url: CONFIG['host'] + '/clients/upload'});
+          uploader.onAfterAddingFile = function(item, filter, options) {
+            if (uploader.queue.length > 1) {
+              uploader.queue.splice(0, 1);
+            }
+          };
+          scope.uploader = uploader;
+        },
+      };
     }
-  })
-;
+  }
+});
+
+app.factory("clients", function($resource) {
+  return $resource(CONFIG['host'] + '/clients');
+});
+
+app.directive('clientsTable', function(clients) {
+  return {
+    restrict: 'E',
+    templateUrl:'components/clients_table.html',
+    compile: function() {
+      return {
+        pre: function(scope, element, attrs) {
+          clients.query(function(data) {
+            scope.clients = data;
+            }, function(err) {
+            console.error("Error occured: ", err);
+          });
+        },
+      };
+    }
+  }
+});
--- client/app/config.js
+++
@@ -1,3 +0,0 @@
-export const CONFIG = {
-  host: ENV['CSV_UPLOADER_API_HOST'] || "http://localhost:3000"
-}
